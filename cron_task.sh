9449ce14cd21fcb3df0cd14077e05ae65fe4bdce
danda <danda@osc.co.cr>
2018-08-23 22:17:12 -0700

    include nodes-by-addr-type.json in auto-commit

--- cron_task.sh
+++ cron_task.sh
@@ -1,3 +1,3 @@
 ./gen_readme.php > README.md && \
-git commit -m "auto regeneration of README.md" README.md && \
+git commit -m "auto regeneration of README.md" README.md nodes-by-addr-type.json && \
 git push
